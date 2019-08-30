670b34bc7d34476e2e17583bdbd3f3825731a27a
Aaryash Raj Sinha <31902160+DirtyVibe@users.noreply.github.com>
2019-08-30 12:33:06 +0530

    30-08-2019

--- MakeCirclesOnCLick.c
+++ MakeCirclesOnCLick.c
@@ -1,7 +1,9 @@
 #include<GL/glut.h> //includes the opengl, glu, and glut header files
 #include<stdlib.h> //includes the standard library header file
 #include<stdbool.h>
+#define pi 3.142857
 
+int i,j;
 float r,g,b,x,y;
 bool check=true;
 
@@ -11,6 +13,7 @@ void circle (void)
 for (r=0.0; r<=1.0; r+=0.001)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+
 	glBegin(GL_POINTS);
 	for ( i = 0; i < (2 * pi); i += 0.0001)
 	{
@@ -30,9 +33,9 @@ void mouse(int button, int state, int mX, int mY)
 
         x = mX;
         y = 480-mY;
-        r=(rand()%9)/8;
-        g=(rand()%9)/8;
-        b=(rand()%9)/8;
+        r=0;
+        g=0;
+        b=0;
         }
 
    else if(button==GLUT_RIGHT_BUTTON && state==GLUT_DOWN)//undo(clear)the drawing
@@ -44,6 +47,14 @@ void mouse(int button, int state, int mX, int mY)
 
      glutPostRedisplay();
      }
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+		case 'f':			//escape key
+			exit(0);
+	}
+}
 
 void display(void)
 {
@@ -69,12 +80,13 @@ int main(int argc,char** argv)
     glutInitWindowSize(640,480);   //sets the width and height of the window in pixels
     glutInitWindowPosition(50,50);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutCreateWindow("Mouse Magic!");
+    glutCreateWindow("GlutMouseFunc.");
 
     glClearColor(1, 1, 1, 0);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glutDisplayFunc(display);
     glutMouseFunc(mouse);
+		glutKeyboardFunc(keyboard);
     glutMainLoop();
 }
