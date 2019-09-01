f64b40009192bcca15b8c4b1be08134e445cee63
Aaryash Raj Sinha <31902160+DirtyVibe@users.noreply.github.com>
2019-09-01 21:00:08 +0530

    01-09-2019

--- ddaLine.c
+++ ddaLine.c
@@ -1,69 +1,65 @@
 #include<GL/glut.h>
 #include<stdlib.h>
 #include<stdio.h>
-float x1,x2,y1,y2;
+float X1,X2,Y1,Y2;
 
-void display(void)
+void Line(void)
 {
-float dy,dx,step,x,y,k,Xin,Yin;
-dx=x2-x1;
-dy=y2-y1;
+	float dy,dx,step,x,y,k,Xin,Yin;
+	dx=X2-X1;
+	dy=Y2-Y1;
 
-if(abs(dx)> abs(dy))
-{
-step = abs(dx);
-}
-else
-step = abs(dy);
+	if(abs(dx)> abs(dy)){
+		step = abs(dx);
+	}
+	else
+		step = abs(dy);
 
-Xin = dx/step;
-Yin = dy/step;
+	Xin = dx/step;
+	Yin = dy/step;
 
-x= x1;
-y=y1;
-glBegin(GL_LINES);
-glVertex2i(x,y);
-glEnd();
+	x= X1;
+	y=X1;
+	glBegin(GL_LINES);
+		glVertex2i(x,y);
+	glEnd();
 
-for (k=1 ;k<=step;k++)
-{
-x= x + Xin;
-y= y + Yin;
+	for (k=1 ;k<=step;k++){
+		x= x + Xin;
+		y= y + Yin;
+		glBegin(GL_POINTS);
+			glVertex2i(x,y);
+		glEnd();
+	}	
 
-glBegin(GL_POINTS);
-glVertex2i(x,y);
-glEnd();
-}
-
-
-glFlush();
+	glFlush();
 }
 
 void init(void)
 {
-glClearColor(0.7,0.7,0.7,0.7);
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-gluOrtho2D(-100,100,-100,100);
+	glClearColor(0.7,0.7,0.7,0.7);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-100,100,-100,100);
 }
 
 int main(int argc, char** argv) {
-printf("Enter the value of x1 : ");
-scanf("%f",&x1);
-printf("Enter the value of y1 : ");
-scanf("%f",&y1);
-printf("Enter the value of x2 : ");
-scanf("%f",&x2);
-printf("Enter the value of y2 : ");
-scanf("%f",&y2);
+	printf("X1 : ");
+	scanf("%f",&X1);
+	printf("Y1 : ");
+	scanf("%f",&Y1);
+	printf("X2 : ");
+	scanf("%f",&X2);
+	printf("Y2 : ");
+	scanf("%f",&Y2);
 
 glutInit(&argc, argv);
 glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
 glutInitWindowSize (500, 500);
 glutInitWindowPosition (100,100);
-glutCreateWindow ("DDA Line Algo");
+glutCreateWindow ("y = mx + c");
 init();
-glutDisplayFunc(display);
+glutDisplayFunc(Line);
 glutMainLoop();
 
 return 0;
