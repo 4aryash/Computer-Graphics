91c3333103201301cbbed53d8627115d25b5553f
Aaryash Raj Sinha <31902160+DirtyVibe@users.noreply.github.com>
2019-09-01 22:59:59 +0530

    01-09-2019

--- locatePoints.c
+++ locatePoints.c
@@ -9,20 +9,12 @@ void init(void){
   gluOrtho2D(0,500,0,500);
 }
 
-void Line(void){
-  glBegin(GL_LINES);
-    glVertex2i(x1,y1);
-    glVertex2i(x2,y2);
-  glEnd();
-
-}
-
 void mousePtPlot(GLint button, GLint action, GLint xMouse, GLint yMouse){
   if(button == GLUT_LEFT_BUTTON && action==GLUT_DOWN)
   {
       glClear(GL_COLOR_BUFFER_BIT);
-      x1=xMouse;
-      y1=500-yMouse;
+      x1 = xMouse;
+      y1 = 500-yMouse;
 
       glPointSize(10);
 
@@ -31,11 +23,33 @@ void mousePtPlot(GLint button, GLint action, GLint xMouse, GLint yMouse){
         glVertex2i(x1,y1);
       glEnd();
       glFlush();
-      //plotpoint(x1,y1);
-      printf("Points: %d,%d\n",x1,y1);
+      printf("Left Click at: (%d,%d)\n",x1,y1);
+  }
+
+  if(button == GLUT_RIGHT_BUTTON && action==GLUT_DOWN)
+  {
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      x2 = xMouse;
+      y2 = 500-yMouse;
+
+      glPointSize(10);
+
+      glBegin(GL_POINTS);
+        glColor3f(1.0,0.01,0.24);
+        glVertex2i(x2,y2);
+      glEnd();
+      glFlush();
+      printf("Right Click at: (%d,%d)\n\n",x2,y2);
   }
 }
 
+void Line(void){
+  glBegin(GL_LINES);
+    glVertex2i(x1,y1);
+    glVertex2i(x2,y2);
+  glEnd();
+}
 
 int main(int argc, char** argv){
   glutInit(&argc, argv);
@@ -45,7 +59,7 @@ int main(int argc, char** argv){
   glutCreateWindow("Locate Points");
 
   init();
-  //glutDisplayFunc(mousePtPlo);
+  glutDisplayFunc(Line);
   glutMouseFunc(mousePtPlot);
   glutMainLoop();
   return 0;
