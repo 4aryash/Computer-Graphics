0e569649a0b5eb9e193bab5f75a3ba931a7439a6
Aaryash Raj Sinha <31902160+DirtyVibe@users.noreply.github.com>
2019-09-02 22:17:43 +0530

    02-09-2019

--- lineFormedThroughMouseClicks.c
+++ lineFormedThroughMouseClicks.c
@@ -6,7 +6,7 @@ void init(void){
   glClearColor(1.0,0.7,0.0,0.0);    //background is set to a shade of yellow
   glMatrixMode(GL_PROJECTION);
   glClear(GL_COLOR_BUFFER_BIT);
-  gluOrtho2D(0,500,0,500);
+  gluOrtho2D(0,600,0,600);
 }
 
 void mousePt(GLint button, GLint action, GLint xMouse, GLint yMouse){
@@ -14,7 +14,7 @@ void mousePt(GLint button, GLint action, GLint xMouse, GLint yMouse){
   {
       //glClear(GL_COLOR_BUFFER_BIT);
       x1 = xMouse;        //x coordinate is formed from the bottom-left screen.
-      y1 = 500-yMouse;    //y coordinate is formed from the top-left screen, so we subtract the window-size from the y-coordinates
+      y1 = 600-yMouse;    //y coordinate is formed from the top-left screen, so we subtract the window-size from the y-coordinates
 
       glPointSize(10);    //size of the point formed
 
@@ -31,7 +31,7 @@ void mousePt(GLint button, GLint action, GLint xMouse, GLint yMouse){
       //glClear(GL_COLOR_BUFFER_BIT);
 
       x2 = xMouse;        //x coordinate is formed from the bottom-left screen.
-      y2 = 500-yMouse;    //y coordinate is formed from the top-left screen, so we subtract the window-size from the y-coordinates
+      y2 = 600-yMouse;    //y coordinate is formed from the top-left screen, so we subtract the window-size from the y-coordinates
 
       glPointSize(10);
 
@@ -56,8 +56,8 @@ void Line(void){
 int main(int argc, char** argv){
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-  glutInitWindowPosition(50,100);
-  glutInitWindowSize(500,500);
+  glutInitWindowPosition(100,100);
+  glutInitWindowSize(600,600);
   glutCreateWindow("Locate Points");
 
   init();
