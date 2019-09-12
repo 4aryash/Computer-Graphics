8049c7f27c4ae4ad13b59b97f18591b05176deee
Aaryash Raj Sinha <31902160+DirtyVibe@users.noreply.github.com>
2019-09-12 23:08:46 +0530

    Update ellipse.c

--- ellipse.c
+++ ellipse.c
@@ -1,7 +1,10 @@
 #include<GL/glut.h>
-#include<stdio.h>
-int x1,y1,x2,y2;
+#include<stdlib.h>
+#include<stdbool.h>
+#include<math.h>
+#define pi 3.142857
 
+float rad, DEG2RAD;
 void init(void){
   glClearColor(1.0,0.7,0.0,0.0);    //background is set to a shade of yellow
   glMatrixMode(GL_PROJECTION);
@@ -9,58 +12,21 @@ void init(void){
   gluOrtho2D(0,600,0,600);
 }
 
-void drawEllipse(float xc, float yc, float x, float y)
+void ellipse(float radiusX, float radiusY)
 {
-  glFlush();
-    int p1[] = {(int)xc+x, (int)yc+y};
-    int p2[] = {(int)xc-x, (int)yc+y};
-    int p3[] = {(int)xc+x, (int)yc-y};
-    int p4[] = {(int)xc-x, (int)yc-y};
-    glVertex2iv(p1);
-    glVertex2iv(p2);
-    glVertex2iv(p3);
-    glVertex2iv(p4);
-}
+   int i;
 
-void ellipseMidpoint(float xc, float yc, float rx, float ry)
-{
-    float rxSq = rx * rx;
-    float rySq = ry * ry;
-    float x = 0, y = ry, p;
-    float px = 0, py = 2 * rxSq * y;
-    drawEllipse(xc, yc, x, y);
+   glBegin(GL_LINE_LOOP);
 
-    p = rySq - (rxSq * ry) + (0.25 * rxSq);
-    while (px < py)
-    {
-        x++;
-        px = px + 2 * rySq;
-        if (p < 0)
-            p = p + rySq + px;
-        else
-        {
-            y--;
-            py = py - 2 * rxSq;
-            p = p + rySq + px - py;
-        }
-        drawEllipse(xc, yc, x, y);
-    }
+   for(i=0;i<360;i++)
+   {
+      rad = i*DEG2RAD;
+      glVertex2f(cos(rad)*radiusX,
+                  sin(rad)*radiusY);
+   }
 
-    p = rySq*(x+0.5)*(x+0.5) + rxSq*(y-1)*(y-1) - rxSq*rySq;
-    while (y > 0)
-    {
-        y--;
-        py = py - 2 * rxSq;
-        if (p > 0)
-            p = p + rxSq - py;
-        else
-        {
-            x++;
-            px = px + 2 * rySq;
-            p = p + rxSq - py + px;
-        }
-        drawEllipse(xc, yc, x, y);
-    }
+   glEnd();
+   glFlush();
 }
 
 int main(int argc, char** argv){
@@ -71,7 +37,7 @@ int main(int argc, char** argv){
   glutCreateWindow("Ellipse");
 
   init();
-  glutDisplayFunc(drawEllipse);    //line function is called
+  glutDisplayFunc(ellipse);    //line function is called
   glutMainLoop();
   return 0;
 }
