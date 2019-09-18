c4c5c79330288bfb5e50bebde3231af8a26a73ca
Aaryash Raj Sinha <31902160+DirtyVibe@users.noreply.github.com>
2019-09-18 23:15:53 +0530

    Update multipleWindows.c

--- multipleWindows.c
+++ multipleWindows.c
@@ -8,7 +8,6 @@ void display(void)
  glClear (GL_COLOR_BUFFER_BIT);
  glColor3f(1.0,0.01,0.24);
  glBegin(GL_POINTS);
- ellipse();
  glEnd();
  glFlush();
  }
@@ -24,14 +23,18 @@ int main(int argc, char** argv)
  glutInit(&argc, argv);
  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
 
- GLint WindowID1, WindowID2;                  // window ID numbers
- glutInitWindowSize(250.0, 250.0);           // set a window size
- glutInitWindowPosition(50,50);              // set a window position
- WindowID1 = glutCreateWindow("Window One"); // Create window 1
+ GLint WindowID1, WindowID2, WindowID3;                  // window ID numbers
+ glutInitWindowSize(868.0, 384.0);           // set a window size
+ glutInitWindowPosition(0,50);              // set a window position
+ WindowID1 = glutCreateWindow("CCTV 1"); // Create window 1
 
- glutInitWindowSize(500.0, 250.0);           // set a window size
+ glutInitWindowSize(868.0, 384.0);           // set a window size
  glutInitWindowPosition(500,50);             // set a window position
- WindowID2 = glutCreateWindow("Window Two"); // Create window 2
+ WindowID2 = glutCreateWindow("CCTV 2"); // Create window 2
+
+ glutInitWindowSize(868.0, 384.0);           // set a window size
+ glutInitWindowPosition(500,100);             // set a window position
+ WindowID3 = glutCreateWindow("CCTV 3"); // Create window 2
 
  init ();
  glutDisplayFunc(display);
